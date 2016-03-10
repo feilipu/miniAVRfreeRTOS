4620a762bee1ea63c07d2352a4a66bd5d170b052
Phillip Stevens <phillip.stevens@gmail.com>
2016-03-10 23:50:34 +1100

    set default system tick to 15ms

--- FreeRTOSBoardDefs.h
+++ FreeRTOSBoardDefs.h
@@ -25,7 +25,7 @@ extern "C" {
 // System Tick - Scheduler timer
 // Prefer to use the Watchdog timer, and choose the rate at which scheduler interrupts will occur.
 
-#define portUSE_WDTO			WDTO_30MS			// portUSE_WDTO to use the Watchdog Timer for xTaskIncrementTick
+#define portUSE_WDTO			WDTO_15MS	// portUSE_WDTO to use the Watchdog Timer for xTaskIncrementTick
 
 /* Watchdog period options: 	WDTO_15MS
 								WDTO_30MS
@@ -35,7 +35,7 @@ extern "C" {
 								WDTO_500MS
 */
 //	xxx Watchdog Timer is 128kHz nominal, but 120 kHz at 5V DC and 25 degrees is actually more accurate, from data sheet.
-#define configTICK_RATE_HZ 			( (TickType_t)( (uint32_t)128000 >> (portUSE_WDTO + 11) ) )  // 2^11 = 2048 WDT scaler for 128kHz Timer
+#define configTICK_RATE_HZ 		( (TickType_t)( (uint32_t)128000 >> (portUSE_WDTO + 11) ) )  // 2^11 = 2048 WDT scaler for 128kHz Timer
 
 #ifdef __cplusplus
 }
