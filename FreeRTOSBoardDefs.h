b97a9c5b4634147aa120b1f35e22a120bb6dd384
Phillip Stevens <phillip.stevens@gmail.com>
2020-02-19 22:33:51 +1100

    FreeRTOS v10.3.0

--- FreeRTOSBoardDefs.h
+++ FreeRTOSBoardDefs.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Phillip Stevens  All Rights Reserved.
+ * Copyright (C) 2020 Phillip Stevens  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -41,26 +41,24 @@
 #ifndef freeRTOSBoardDefs_h
 #define freeRTOSBoardDefs_h
 
-#include <avr/io.h>
-#include <avr/wdt.h>
-
-#include "task.h"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#include <avr/io.h>
+#include <avr/wdt.h>
+
 // System Tick - Scheduler timer
 // Use the Watchdog timer, and choose the rate at which scheduler interrupts will occur.
 
-#define portUSE_WDTO        WDTO_15MS    // portUSE_WDTO to use the Watchdog Timer for xTaskIncrementTick
+#define portUSE_WDTO            WDTO_15MS    // portUSE_WDTO to use the Watchdog Timer for xTaskIncrementTick
 
-/* Watchdog period options: WDTO_15MS
-                            WDTO_30MS
-                            WDTO_60MS
-                            WDTO_120MS
-                            WDTO_250MS
-                            WDTO_500MS
+/* Watchdog period options:     WDTO_15MS
+                                WDTO_30MS
+                                WDTO_60MS
+                                WDTO_120MS
+                                WDTO_250MS
+                                WDTO_500MS
 */
 //    xxx Watchdog Timer is 128kHz nominal, but 120 kHz at 5V DC and 25 degrees is actually more accurate, from data sheet.
 #define configTICK_RATE_HZ  ( (TickType_t)( (uint32_t)128000 >> (portUSE_WDTO + 11) ) )  // 2^11 = 2048 WDT scaler for 128kHz Timer
